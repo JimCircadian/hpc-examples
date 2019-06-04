51ed9b85d685a20aac5312e9e8793b9fe8d3cec4
James Byrne <jambyr@bas.ac.uk>
2019-06-04 16:25:20 +0100

    Example job runs via SLURM

--- node_counter.py
+++ node_counter.py
@@ -1,36 +1,48 @@
 #!/usr/bin/env python
 
 from mpi4py import MPI
+import argparse
 import logging
 import sys
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
-i = 0
-m = 1024
-c = 0
+if __name__ == "__main__":
+    a = argparse.ArgumentParser()
+    a.add_argument("m", help="Max number to count to", type=int)
+    a.add_argument("-v", "--verbose", default=False, action="store_true")
+    args = a.parse_args()
+    m = args.m
 
-while i < m:
-    if rank == 0 and i == 0:
-        i += 1
-        c += 1
-        comm.send(i, dest=rank+1)
-        continue
-
-    if rank == 0:
-        i = comm.recv(source=size-1)
+    if args.verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
     else:
-        i = comm.recv(source=rank-1)
-    logging.warn("{} received {}".format(rank, i))
+        logging.getLogger().setLevel(logging.INFO)
 
-    if i < m:
-        i += 1
-        c += 1
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    i = 0
+    c = 0
 
-    if rank == size - 1:
-        comm.send(i, dest=0)
-    else:
-        comm.send(i, dest=rank+1)
+    while i < m:
+        if rank == 0 and i == 0:
+            i += 1
+            c += 1
+            comm.send(i, dest=rank+1)
+            continue
 
-logging.warn("{} made {} counts towards {}".format(rank, i, m))
+        if rank == 0:
+            i = comm.recv(source=size-1)
+        else:
+            i = comm.recv(source=rank-1)
+        logging.debug("{} received {}".format(rank, i))
+
+        if i < m:
+            i += 1
+            c += 1
+
+        if rank == size - 1:
+            comm.send(i, dest=0)
+        else:
+            comm.send(i, dest=rank+1)
+
+    logging.info("{} made {} counts towards {}".format(rank, c, m))
--- run.sh
+++ run.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 #
 # Output directory
-#SBATCH --output=/data/hpcdata/users/jambyr/logs/job.%j.%N.out
-#SBATCH --chdir=/data/hpcdata/users/jambyr/
+#SBATCH --output=/data/hpcdata/users/jambyr/logs/tester.%j.%N.out
+#SBATCH --chdir=/data/hpcdata/users/jambyr/tester
 #SBATCH --mail-type=begin,end,fail,requeue
 #SBATCH --mail-user=jambyr@bas.ac.uk
 #SBATCH --time=00:05:00
 #SBATCH --partition=quick
 #SBATCH --nodes=1-2
+#SBATCH --job-name=tester
 #
-# Load the modules
-source /etc/profile.d/modules.sh
-module load hpc/python/conda-python-3.6.4
 
-mpirun -np $SLURM_NTASKS $1
+NUM=${1:-128}
+NP=${2:-8}
+
+module load hpc/mpich/gcc/3.2
+source /data/hpcdata/users/jambyr/tester/venv/bin/activate
+
+mpirun -np $NP python /data/hpcdata/users/jambyr/tester/node_counter.py $NUM
 

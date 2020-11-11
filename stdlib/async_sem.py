3b0e3d15fc4a3d2a6a2da40e7c32ecbb243a4f30
Jim Circadian <jamescircadian@gmail.com>
2020-11-11 19:59:46 +0000

    Extra asyncio messing about

--- stdlib/async_sem.py
+++ stdlib/async_sem.py
@@ -1,5 +1,7 @@
 import asyncio
+import concurrent.futures as cf
 import datetime as dt
+import os
 import sys
 
 
@@ -16,45 +18,66 @@ async def sem_batcher(n, tasks):
 async def async_sleep(i):
     print("Started {}".format(i))
     # This doesn't block
-    await asyncio.sleep(5)
+    await asyncio.sleep(2)
     print("End {}".format(i))
 
 
 # Thank you: https://stackoverflow.com/questions/16071866/non-blocking-subprocess-call
 async def async_cmd(i):
     print("Started {}".format(i))
-    # But this does, what can I do
-    proc = await asyncio.create_subprocess_exec('sleep', '5')
+    proc = await asyncio.create_subprocess_exec('sleep', '2')
     rc = await proc.wait()
     print("End {}".format(i))
 
 
-def batch_runs(func):
+async def async_bash(i):
+    print("Started {}".format(i))
+    proc = await asyncio.create_subprocess_shell('./sleep.sh {}'.format(i), cwd="subdir")
+    rc = await proc.wait()
+    print("End {}".format(i))
+
+
+def batch_runs(loop, func):
     tasks = list()
 
-    loop = asyncio.get_event_loop()
-
-    for i in range(50):
+    for i in range(10):
         tasks.append(func(i))
 
+    loop.run_until_complete(sem_batcher(15, tasks))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2 and sys.argv[1].startswith("async_") and \
+            hasattr(sys.modules['__main__'], sys.argv[1]):
+        func = getattr(sys.modules['__main__'], sys.argv[1])
+    else:
+        func = async_cmd
+
+    print("""asyncio semaphore limiter test {}""".format(func.__name__))
+
+    # timeit seems to get a bit grumpy
+    st = dt.datetime.now()
+    num = 5
+    loop = asyncio.get_event_loop()
+
     try:
-        loop.run_until_complete(sem_batcher(15, tasks))
+        # with cf.ThreadPoolExecutor() as executor:
+        #     for i in range(num):
+        #         futures = executor.submit(batch_runs, loop, func)
+        # cf.as_completed(futures)
+
+        # This will allow sequential execution of "batches", but not parallelised as above which only runs asyncio
+        # TODO: BUT I cannot find a reliable description of the inheritence of process to subprocess cwd....
+        #  This is what i want to test!
+        for i in range(num):
+            orig = os.getcwd()
+            batch_runs(loop, func)
+            os.chdir(orig)
+
     finally:
         loop.shutdown_asyncgens()
         loop.close()
 
-
-if __name__ == "__main__":
-    print("""asyncio semaphore limiter test""")
-
-    if len(sys.argv) == 2 and sys.argv[1] == "asyncio":
-        func = async_sleep
-    else:
-        func = async_cmd
-
-    # timeit seems to get a bit grumpy
-    st = dt.datetime.now()
-    batch_runs(func)
     en = dt.datetime.now()
     print("Elapsed: {} seconds".format((en - st).total_seconds()))
 
